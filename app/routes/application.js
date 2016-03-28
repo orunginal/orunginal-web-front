39557ac9fcd94d886f0ee013efd61a1969d3b5bb
Quentin Bayart <quentin.bayart45430@gmail.com>
2016-03-28 18:49:20 +0200

    Configure user widget

--- app/routes/application.js
+++ app/routes/application.js
@@ -3,20 +3,4 @@ import ApplicationRouteMixin from 'ember-simple-auth/mixins/application-route-mi
 
 export default Ember.Route.extend(ApplicationRouteMixin, {
 
-	beforeModel(transition){
-		this._super(transition);
-
-		/*====================================
-	    Show Menu on Book
-	    ======================================*/
-	    Ember.$(window).bind('scroll', function() {
-	        var navHeight = $(window).height() - 150;
-	        if (Ember.$(window).scrollTop() > navHeight) {
-	            Ember.$('.navbar-default').addClass('on');
-	        } else {
-	            Ember.$('.navbar-default').removeClass('on');
-	        }
-	    });
-	}
-	 
 });
